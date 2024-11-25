f4a6456e42034e860214de0dd98bde2f57c7b684
Sagun Bhandari <sagunbhandari123@gmail.com>
2024-11-25 12:07:30 +0545

    initalize project

---
+++ app.js
@@ -0,0 +1,8 @@
+const child = React.createElement(
+    'div',
+    { id: 'child' }, [
+    React.createElement('h1', { id: 'heading' }, 'Hello World'), 
+    React.createElement('h1', { id: 'heading' }, 'Hello World2')]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(child);
